85679ce8c14484f97a3eb92d0e6c77e3752d7c8f
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-07-27 22:46:15 +0300

    22/46____________27/07/23

--- App.js
+++ App.js
@@ -1,40 +1,41 @@
+import React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { useFonts } from "expo-font";
-import Home from "./src/Screens/Home";
-import LoginScreen from "./src/Screens/LoginScreen";
-import RegistrationScreen from "./src/Screens/RegistrationScreen";
-import ProfileScreen from "./src/Screens/ProfileScreen";
-import CreatePostsScreen from "./src/Screens/CreatePostsScreen";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+
+import LoginScreen from "./Screens/AuthPages/LoginScreen";
+import RegistrationScreen from "./Screens/AuthPages/RegistrationScreen";
+import Home from "./Screens/Home";
+
+const MainStack = createStackNavigator();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    RobotoBold: require("./src/fonts/Roboto-Bold.ttf"),
-    RobotoRegular: require("./src/fonts/Roboto-Regular.ttf"),
-    RobotoMedium: require("./src/fonts/Roboto-Medium.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
-  const Stack = createStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="LoginScreen"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen
-          name="RegistrationScreen"
-          component={RegistrationScreen}
-        />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="CreatePostsScreen" component={CreatePostsScreen} />
-        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <NavigationContainer>
+        <MainStack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
+        >
+          <MainStack.Screen
+            name="Regestration"
+            component={RegistrationScreen}
+          />
+          <MainStack.Screen name="Login" component={LoginScreen} />
+          <MainStack.Screen name="Home" component={Home} />
+        </MainStack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
